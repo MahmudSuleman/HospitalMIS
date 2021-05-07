0440a3c188e7863151c309d469d90c5de9c5e983
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-05-07 09:32:38 +0000

    add auth scafolding

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mannat Themes">
+    <meta name="keyword" content="">
+
+    <title>Hospital MIS | </title>
+
+    <!-- Theme icon -->
+    <link rel="shortcut icon" href="/syntra/assets/images/favicon.ico">
+
+    <link href="/syntra/assets/plugins/morris-chart/morris.css" rel="stylesheet">
+    <!-- Theme Css -->
+    <link href="/syntra/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/syntra/assets/css/slidebars.min.css" rel="stylesheet">
+    <link href="/syntra/assets/css/icons.css" rel="stylesheet">
+    <link href="/syntra/assets/css/menu.css" rel="stylesheet" type="text/css">
+    <link href="/syntra/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body class="sticky-header">
+<section>
+    <!-- sidebar left start-->
+    <div class="sidebar-left">
+        <div class="sidebar-left-info">
+
+            <div class="user-box">
+                <div class="d-flex justify-content-center">
+                    <img src="/syntra/assets/images/users/avatar-1.jpg" alt="" class="img-fluid rounded-circle">
+                </div>
+                <div class="text-center text-white mt-2">
+                    <h6>Travis Watson</h6>
+                    <p class="text-muted m-0">Admin</p>
+                </div>
+            </div>
+
+            <!--sidebar nav start-->
+            <ul class="side-navigation">
+
+
+                <li>
+                    <h3 class="navigation-title">Components</h3>
+                </li>
+
+                <li><a href="#"><i class="mdi mdi-diamond"></i> <span>Icons</span></a>
+
+                </li>
+                <li class="menu-list"><a href="javascript:;"><i class="mdi mdi-table"></i> <span>Tables</span></a>
+                    <ul class="child-list">
+                        <li><a href="tables-basic.html"> Basic Table</a></li>
+
+                    </ul>
+                </li>
+
+
+            </ul><!--sidebar nav end-->
+        </div>
+    </div><!-- sidebar left end-->
+
+    <!-- body content start-->
+    <div class="body-content">
+        <!-- header section start-->
+        <div class="header-section">
+            <!--logo and logo icon start-->
+            <div class="logo">
+                <a href="#">
+                {{--                    logo for the system below --}}
+                {{--                            <span class="logo-img">--}}
+                {{--                                <img src="" alt="" height="26">--}}
+                {{--                            </span>--}}
+                <!--<i class="fa fa-maxcdn"></i>-->
+                    <span class="brand-name">Hospital MIS</span>
+                </a>
+            </div>
+
+            <!--toggle button start-->
+            <a class="toggle-btn"><i class="ti ti-menu"></i></a>
+            <!--toggle button end-->
+
+
+            <div class="notification-wrap">
+                <!--right notification start-->
+                <div class="right-notification">
+                    <ul class="notification-menu">
+                        <li>
+                            <a href="javascript:;" data-toggle="dropdown">
+                                <img src="/syntra/assets/images/users/avatar-1.jpg" alt="">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right profile-menu">
+                                <a class="dropdown-item" href="#"><i
+                                        class="mdi mdi-account-circle m-r-5 text-muted"></i> Profile</a>
+                                <a class="dropdown-item" href="#"><span
+                                        class="badge badge-success pull-right">5</span><i
+                                        class="mdi mdi-settings m-r-5 text-muted"></i> Settings</a>
+                                <a class="dropdown-item" href="#"><i
+                                        class="mdi mdi-lock-open-outline m-r-5 text-muted"></i> Lock screen</a>
+                                <a class="dropdown-item" href="#"><i class="mdi mdi-logout m-r-5 text-muted"></i> Logout</a>
+                            </div>
+
+                        </li>
+
+
+                    </ul>
+                </div><!--right notification end-->
+            </div>
+        </div>
+        <!-- header section end-->
+
+        <div class="container-fluid">
+            <div class="page-head">
+                <h4 class="mt-2 mb-2">Dashboard</h4>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!--end row-->
+
+
+        </div><!--end container-->
+
+        <!--footer section start-->
+        <footer class="footer">
+            2018 &copy; Bright G.
+        </footer>
+        <!--footer section end-->
+
+
+    </div>
+    <!--end body content-->
+</section>
+
+<!-- jQuery -->
+<script src="/syntra/assets/js/jquery-3.2.1.min.js"></script>
+<script src="/syntra/assets/js/popper.min.js"></script>
+<script src="/syntra/assets/js/bootstrap.min.js"></script>
+<script src="/syntra/assets/js/jquery-migrate.js"></script>
+<script src="/syntra/assets/js/modernizr.min.js"></script>
+<script src="/syntra/assets/js/jquery.slimscroll.min.js"></script>
+<script src="/syntra/assets/js/slidebars.min.js"></script>
+
+<!--plugins js-->
+<script src="/syntra/assets/plugins/counter/jquery.counterup.min.js"></script>
+<script src="/syntra/assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+<script src="/syntra/assets/plugins/sparkline-chart/jquery.sparkline.min.js"></script>
+<script src="/syntra/assets/pages/jquery.sparkline.init.js"></script>
+
+<script src="/syntra/assets/plugins/chart-js/Chart.bundle.js"></script>
+<script src="/syntra/assets/plugins/morris-chart/raphael-min.js"></script>
+<script src="/syntra/assets/plugins/morris-chart/morris.js"></script>
+{{--<script src="/syntra/assets/pages/dashboard-init.js"></script>--}}
+
+
+<!--app js-->
+<script src="/syntra/assets/js/jquery.app.js"></script>
+
+</body>
+</html>
