eff8073549f916abf05c58cf97d1f1d6bbbd8b99
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-05-20 11:45:47 +0000

    set up sqlite database file to heroku

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.master');
-});
+})->middleware(['auth']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
