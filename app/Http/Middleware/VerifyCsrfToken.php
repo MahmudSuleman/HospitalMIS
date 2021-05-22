22ffcd9388bb2be6b9160ff4e1d039b6731a96f6
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-05-22 02:49:46 +0000

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'http://hospital-miss-app.herokuapp.com'
+        'http://hospital-miss-app.herokuapp.com/*'
     ];
 }
