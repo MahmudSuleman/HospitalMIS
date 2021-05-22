f21407bd21b29587b239260bf36baf296a8a12d1
Mahmud Chamtooni <mudisheikh@outlook.com>
2021-05-22 05:19:11 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth']);
+Route::get('/user', function (){
+    dd(User::all());
+});
+
